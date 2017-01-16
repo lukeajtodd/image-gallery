f68dcc437abf45b9ade4765c75aaf6d58a804076
Luke Todd <luke.todd@plus.net>
2017-01-16 14:58:05 +0000

    Tweaks for responsive-ness

--- src/TitleCard.js
+++ src/TitleCard.js
@@ -1,20 +1,8 @@
 import { h } from 'preact';
 
 const TitleCard = () => {
-
-  const border = "4px solid #000";
-  const borderBottomFix = "-2px";
-
-  const cardStyles = {
-    background: '#fff',
-    borderTop: border,
-    borderRight: border,
-    borderBottom: border,
-    marginBottom: borderBottomFix,
-  }
-
   return (
-    <div style={cardStyles} className="card">
+    <div style="background: #fff" className="card">
       <div className="logo__container">
         <span class="logo_L">L</span>
         <span class="logo_T">T</span>
